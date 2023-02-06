4f98b831dc0fb49e101f0226aaae4d5ed71960d7
toinoublanc <93349536+toinoublanc@users.noreply.github.com>
2023-02-06 12:36:24 +0100

    Update repo

---
+++ api/processing/processing.py
@@ -0,0 +1,11 @@
+from tqdm import tqdm
+import re
+
+def clean_text(text):
+    cleaned_text=[]
+    for sentence in tqdm(text):
+        sentence=sentence.lower()
+        sentence=re.sub('https?://[^\s<>"]+|www\.[^\s<>"]+',' ',sentence)
+        sentence=re.sub('[^0-9a-z]',' ',sentence)
+        cleaned_text.append(sentence)
+    return cleaned_text
